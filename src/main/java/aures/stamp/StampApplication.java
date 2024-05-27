2edd667461b7e6902c9bf1d39b868f5cac955f4c
CHOUABBIA-AMINE <chouabbia.amine@gmail.com>
2024-05-27 13:59:29 +0100

    initial commit

---
+++ src/main/java/aures/stamp/StampApplication.java
@@ -0,0 +1,13 @@
+package aures.stamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StampApplication.class, args);
+	}
+
+}
