2b0a1ba5eefeea3cb000b39d6a42504a6c448ae5
CHOUABBIA-AMINE <chouabbia.amine@gmail.com>
2024-05-27 15:15:50 +0100

    Successfull source connection

--- src/main/java/aures/stamp/StampApplication.java
+++ src/main/java/aures/stamp/StampApplication.java
@@ -2,8 +2,10 @@ package aures.stamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:application.properties")
 public class StampApplication {
 
 	public static void main(String[] args) {
